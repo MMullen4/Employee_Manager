7f37e39d4081b133160eec1555f4ca6733024354
Matt Mullen <mhmullen@icloud.com>
2025-02-25 14:42:09 -0800

    created schemas

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,32 @@
-DROP DATABASE IF EXISTS books_db;
-CREATE DATABASE books_db;
+DROP DATABASE IF EXISTS employees;
+CREATE DATABASE employees;
 
-\c books_db;
+USE employees;
 
-CREATE TABLE favorite_books (
-  id SERIAL PRIMARY KEY,
-  section INTEGER NOT NULL,
-  book_name VARCHAR(30) NOT NULL,
-  in_stock BOOLEAN,
-  quantity INTEGER NOT NULL
+CREATE TABLE department (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, --prevent id from being neg# & incr each new row
+  name VARCHAR(30) UNIQUE NOT NULL
+);
+
+CREATE TABLE role (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, -- only positive #'s
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INTEGER NOT NULL,
+  INDEX dep_ind (department_id) NOT NULL,
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE -- will delete all roles with department if department is deleted
+);
+
+CREATE TABLE employee (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT UNSIGNED NOT NULL,
+  INDEX role_id (roll_id) NOT NULL,
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+  -- if role is deleted all records with that role_id will aslo be deleted
+  manager_id INT UNSIGNED,  -- only positive #'s
+  INDEX man_ind (manager_id),
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL -- if manager is deleted, set manager to null
+
 );
